9afe5dc1f01de05be42dc3101c159db9130a97b9
Kevin <kevinpintueles@gmail.com>
2023-03-03 16:03:21 -0600

    redy

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,11 +1,13 @@
 describe('Prueba 1', () => {
-    it('primer prueba', async () => {
-        await browser.url('https://webdriver.io/')
+    it('primer prueba', async function () {
+        await browser.url('https://webdriver.io/');
 
-          //assert title
-          console.log('Test');
-        await expect(browser).toHaveTitleContaining('WebdriverIO')
-    });
+        await browser.maximizeWindow()
 
+        //assert title
+        console.log('Test');
+        await expect(browser).toHaveTitleContaining('WebdriverIO');
+        
+      });
+      
 });
-
