a088eb59f8e59a1b93ebc4653e2374407d96b8cc
Kevin <kevinpintueles@gmail.com>
2023-03-03 18:08:15 -0600

    prueb login

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,3 +1,5 @@
+import loginPage from "../pageobjects/login.page";
+
 describe('Prueba 1', () => {
     it('primer prueba', async function () {
         await browser.url('https://webdriver.io/');
@@ -8,6 +10,15 @@ describe('Prueba 1', () => {
         console.log('Test');
         await expect(browser).toHaveTitleContaining('WebdriverIO');
         
+        
+        //Click botton
+        const button1 = await $('=Get Started');
+        await button1.click(); 
+        browser.pause(10000);           
+
+
+         loginPage.login('test', '1223345567'). 
+
       });
       
 });
